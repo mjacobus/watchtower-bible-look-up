2633b07eef60739a0fd059d51283b24bf834594a
Marcelo Guilherme Jacobus Jr <marcelo.jacobus@gmail.com>
2012-12-22 22:17:58 -0200

    Changed the display format

--- src/WatchtowerLookup.js
+++ src/WatchtowerLookup.js
@@ -1,6 +1,6 @@
 $('body').append('<script src="https://raw.github.com/mjacobus/watchtower-bible-look-up/development/src/BibleReference.js" type="text/javascript"></script>');
 
-var content = [$('#article h1').text()];
+var references = [];
 
 $('.bodyTxt a.jsBibleLink').each(function(textNumber){
     var href = $(this).attr('href');
@@ -14,10 +14,19 @@ $('.bodyTxt a.jsBibleLink').each(function(textNumber){
             var selector = '#v-' + verses[i];
             vs.push(html.find(selector).text());
         }
+        r.setContent(vs.join(" "));
     }});
     
-    content.push(r.getText());
-    content.push(vs.join(" "));
+    references.push(r);
 });
 
-alert(content.join("\n"));
+var content = [['<h1>',$('#article h1').text(), '</h1>'].join('')]
+var r;
+for(var i in references) {
+    r = references[i];
+    content.push(['<h2>',r.getText(),'</h2>'].join(''));
+    content.push(['<p>',r.getContent(),'</p>'].join(''));
+}
+
+$('#extracted_texts').remove();
+$('body').prepend('<div id="extracted_texts">' + content.join("") +'</div>');
